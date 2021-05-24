0507033b7080db92ff12bfafead868de16d0984c
Andras Bacsai <andras.bacsai@gmail.com>
2021-05-24 14:32:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fastify = require('fastify')({ logger: {level: "error"}, trustProxy: true 
 fastify.register(require('fastify-cors'))
 
 fastify.get('/', function (req, reply) {
-  console.log(req.raw)
+  console.log(req.headers['x-real-ip'])
   return {}
 })
 

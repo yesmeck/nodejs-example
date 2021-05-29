cc8de80d78e19fd0e18e767412ef89087920f2d5
Andras Bacsai <andras.bacsai@gmail.com>
2021-05-30 00:10:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fastify = require('fastify')({ logger: {level: "error"}, trustProxy: true 
 fastify.register(require('fastify-cors'))
 
 fastify.get('/', function (req, reply) {
-  return {hello: "world"}
+  return {hello: "video"}
 })
 
 const start = async () => {

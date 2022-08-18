39487e762211064f0d6ef727fecf75a8f470cb7a
Andras Bacsai <andras.bacsai@gmail.com>
2022-08-18 16:27:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fastify = require('fastify')({ logger: {level: "error"}, trustProxy: true })
 
+const PORT = process.env.PORT || 3000;
+
 fastify.register(require('fastify-cors'))
 
 fastify.get('/', function (req, reply) {
@@ -9,7 +11,7 @@ fastify.get('/', function (req, reply) {
 
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(PORT, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
